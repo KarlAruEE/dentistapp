4b98c9c8c4c5a18efab86ec52ab9e47fe6390155
KarlAru <kiri@karlaru.ee>
2022-12-17 23:45:31 +0200

    Optimize imports

--- src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
+++ src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
@@ -15,9 +15,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.time.LocalDate;
-import java.util.Date;
-
 @Controller
 @RequiredArgsConstructor
 @Data
--- src/main/java/com/cgi/dentistapp/entities/repositories/DentistVisitRepository.java
+++ src/main/java/com/cgi/dentistapp/entities/repositories/DentistVisitRepository.java
@@ -2,7 +2,6 @@ package com.cgi.dentistapp.entities.repositories;
 
 import com.cgi.dentistapp.entities.DentistVisit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
